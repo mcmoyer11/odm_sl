e5869c80a51601295fda1f9e4af39cda311ea29a
Bruce Tesar <tesar@rutgers.edu>
2015-07-10 13:56:19 -0400

    Removed some now unnecessary Cucumber directory hooks.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,7 +1,4 @@
 # Hooks for the Cucumber scenarios.
 
 Before do
-  @project_dir = File.expand_path('../../..',__FILE__)
-  @test_dir = File.join(@project_dir,'test')
-  @fixture_dir = File.join(@test_dir,'fixtures')
 end
