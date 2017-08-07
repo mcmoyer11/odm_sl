b06884c3837dde46cafe83a15b31541a6ea4ef1d
Bruce Tesar <tesar@rutgers.edu>
2017-08-07 03:59:16 -0400

    Added a few specs for LoserSelector_by_ranking.

--- spec/loserselector_by_ranking_spec.rb
+++ spec/loserselector_by_ranking_spec.rb
@@ -7,60 +7,39 @@ RSpec.describe LoserSelector_by_ranking do
   before(:each) do
     @winner = instance_double(Candidate, "winner")
     @input = "input"
-    allow(@winner).to receive(:input).and_return(@input)
+    # Mock: retrieve the input of the winner
+    expect(@winner).to receive(:input).and_return(@input)
     @loser = instance_double(Candidate, "loser")
     @sys = double("system")
-    @hypothesis = instance_double(Hypothesis, "hypothesis")
     @erc_list = double("erc list")
+    competition = instance_double(Competition, "competition")
+    # Mock: compute the competition for the input
+    expect(@sys).to receive(:gen).with(@input).and_return(competition)
+    @rcd_class = double("rcd class")
+    rcd_object = double("rcd object")
+    # Mock: construct an Rcd object
+    expect(@rcd_class).to receive(:new).with(@erc_list).and_return(rcd_object)
+    hierarchy = instance_double(Hierarchy, "hierarchy")
+    # Mock: construct a constraint hierarchy
+    expect(rcd_object).to receive(:hierarchy).and_return(hierarchy)
+    @optimizer = class_double(MostHarmonic, "optimizer class")
+    @mh = instance_double(MostHarmonic,"optimizer object")
+    # Mock: compute the optimal candidates
+    expect(@optimizer).to receive(:new).with(competition,hierarchy).and_return(@mh)
   end
   context "given a winner that is the sole optimum" do
     before(:each) do
-      competition = instance_double(Competition)
-      optima_list = [@winner]
-      allow(@sys).to receive(:gen).with(@input).and_return(optima_list)
-      allow(@hypothesis).to receive(:erc_list).and_return(@erc_list)
-      rcd_class = double("rcd class")
-      rcd_object = double("rcd object")
-      allow(rcd_class).to receive(:new).with(@erc_list).and_return(rcd_object)
-      hierarchy = instance_double(Hierarchy)
-      allow(rcd_object).to receive(:hierarchy).and_return(hierarchy)
-      optimizer = class_double(MostHarmonic, "optimizer")
-      @select_loser_by_ranking = LoserSelector_by_ranking.new(@sys, rcd_class: rcd_class, optimizer_class: optimizer)
+      # NOTE: this is NOT testing the logic of the block given to mh.find
+      allow(@mh).to receive(:find).and_return(nil)
+      @select_loser_by_ranking = LoserSelector_by_ranking.new(@sys, rcd_class: @rcd_class, optimizer_class: @optimizer)
     end
-
-    it "computes the constraint hierarchy"
-    it "computes the competition for the input"
-    it "finds the optimal candidates"
     it "returns nil" do
-      pending
-      expect(@select_loser_by_ranking.select_loser(@winner,@hypothesis.erc_list)).to be nil
+      expect(@select_loser_by_ranking.select_loser(@winner,@erc_list)).to be nil
     end
   end
 
-  context "given a winner that is not the sole optimum" do
-    before(:each) do
-      optima_list = [@loser]
-      allow(@sys).to receive(:gen).with(@input).and_return(optima_list)
-      allow(@hypothesis).to receive(:erc_list).and_return(@erc_list)
-      rcd_class = double("rcd class")
-      rcd_object = double("rcd object")
-      allow(rcd_class).to receive(:new).with(@erc_list).and_return(rcd_object)
-      allow(rcd_object).to receive(:hierarchy)
-      optimizer = class_double(MostHarmonic, "optimizer")
-      @select_loser_by_ranking = LoserSelector_by_ranking.new(@sys, rcd_class: rcd_class, optimizer_class: optimizer)
-    end
-    it "computes the ranking"
-    it "finds the optimal candidates"
-    it "returns the sole optimum" do
-      pending
-      expect(@select_loser_by_ranking.select_loser(@winner,@hypothesis.erc_list)).to eq @loser
-    end
-  end
+  context "given a winner that is not the sole optimum"
   
-  context "given a winner not contained in the set of optima" do
-    it "computes the ranking"
-    it "finds the optimal candidates"
-    it "returns one of the optima"
-  end
+  context "given a winner not contained in the set of optima"
 end
 
