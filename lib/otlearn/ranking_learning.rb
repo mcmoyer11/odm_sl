ee7622c90d036022f604b9787eae9e5f3230a622
Bruce Tesar <tesar@rutgers.edu>
2017-08-14 01:05:43 -0400

    Replaced references to hypothesis with refs for grammar in ranking_learning.rb.

--- lib/otlearn/ranking_learning.rb
+++ lib/otlearn/ranking_learning.rb
@@ -8,42 +8,42 @@ require_relative 'mrcd'
 module OTLearn
   
   # Performs ranking learning on the given word list, using the
-  # given hypothesis. The parameter _hypothesis_ is directly updated
+  # given grammar. The parameter +grammar+ is directly updated
   # with the additional winner-loser pairs obtained. Loser selection is
   # done using the Faith Low ranking bias.
   # 
   # Returns true if any change at all is made to
-  # the hypothesis (any new winner-loser pairs are added).
+  # the grammar (any new winner-loser pairs are added).
   # 
   # The underlying form for each word is set so that each feature matches
   # the underlying value if it is set in the lexicon, and otherwise
   # matches the surface value of the feature in the word.
-  def OTLearn::ranking_learning_faith_low(word_list, hypothesis)
+  def OTLearn::ranking_learning_faith_low(word_list, grammar)
     winner_list = data_dup_and_match_output(word_list)
     # Use the faith-low ranking bias for ranking learning
-    selector = LoserSelector_by_ranking.new(hypothesis.system, rcd_class: OTLearn::RcdFaithLow)
-    mrcd_result = Mrcd.new(winner_list, hypothesis, selector)
-    mrcd_result.added_pairs.each { |pair| hypothesis.add_erc(pair) }
+    selector = LoserSelector_by_ranking.new(grammar.system, rcd_class: OTLearn::RcdFaithLow)
+    mrcd_result = Mrcd.new(winner_list, grammar, selector)
+    mrcd_result.added_pairs.each { |pair| grammar.add_erc(pair) }
     return mrcd_result.any_change?
   end
 
   # Performs ranking learning on the given word list, using the
-  # given hypothesis. The parameter _hypothesis_ is directly updated
+  # given grammar. The parameter +grammar+ is directly updated
   # with the additional winner-loser pairs obtained. Loser selection is
   # done using the Mark Low ranking bias.
   # 
   # Returns true if any change at all is made to
-  # the hypothesis (any new winner-loser pairs are added).
+  # the grammar (any new winner-loser pairs are added).
   # 
   # The underlying form for each word is set so that each feature matches
   # the underlying value if it is set in the lexicon, and otherwise
   # matches the surface value of the feature in the word.  
-  def OTLearn::ranking_learning_mark_low(word_list, hypothesis)
+  def OTLearn::ranking_learning_mark_low(word_list, grammar)
     winner_list = data_dup_and_match_output(word_list)
     # Use the mark-low ranking bias for ranking learning
-    selector = LoserSelector_by_ranking.new(hypothesis.system, rcd_class: OTLearn::RcdMarkLow)
-    mrcd_result = Mrcd.new(winner_list, hypothesis, selector)
-    mrcd_result.added_pairs.each { |pair| hypothesis.add_erc(pair) }
+    selector = LoserSelector_by_ranking.new(grammar.system, rcd_class: OTLearn::RcdMarkLow)
+    mrcd_result = Mrcd.new(winner_list, grammar, selector)
+    mrcd_result.added_pairs.each { |pair| grammar.add_erc(pair) }
     return mrcd_result.any_change?    
   end
   
