9229133ec8c5d68918ce9f2188089a4d4127a697
Bruce Tesar <tesar@rutgers.edu>
2018-05-31 19:51:25 -0400

    Created skeleton of spec for FewestSetFeatures.

---
+++ spec/otlearn/fewest_set_features_spec.rb
@@ -0,0 +1,14 @@
+# Author: Bruce Tesar
+
+require_relative '../../lib/otlearn/fewest_set_features'
+
+RSpec.describe OTLearn::FewestSetFeatures do
+  context "in this context" do
+    before(:each) do
+      @fewest_set_features = OTLearn::FewestSetFeatures.new
+    end
+    it "does this"
+  end
+
+end # RSpec.describe OTLearn::FewestSetFeatures
+
