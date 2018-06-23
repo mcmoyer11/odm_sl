66dd74d28cae306daa41c6e5bf25db86f75ee3fc
Bruce Tesar <tesar@rutgers.edu>
2018-06-23 18:40:47 -0400

    Refactored RCD_image to simplify internal structure.

--- lib/rcd_image.rb
+++ lib/rcd_image.rb
@@ -1,6 +1,7 @@
 # Author: Bruce Tesar
 # 
 
+require_relative "ercs_image"
 require_relative "ct_image"
 require_relative "hierarchy"
 
@@ -15,14 +16,8 @@ class RCD_image < CT_image
   attr_reader :rcd_result
 
   # Constructs a new RCD_image from an rcd_results object.
-  #
-  # ==== Parameters
-  #
-  # * +rcd_result+ - the result of an RCD execution (e.g., class Rcd).
-  #
-  # ==== Examples
-  #
-  # RCD_image.new(rcd_result)
+  # 
+  # +rcd_result+ - the result of an RCD execution (e.g., class Rcd).
   def initialize(rcd_result)
     @rcd_result = rcd_result
     ercs, constraints = construct_ercs_and_constraints
@@ -36,13 +31,13 @@ class RCD_image < CT_image
   #   construct_ercs_and_constraints() -> [sorted_ercs, sorted_constraints]
   def construct_ercs_and_constraints
     # Add the unranked constraints as a "final stratum" to the hierarchy.
-    @hier_with_unranked = Hierarchy.new
-    @hier_with_unranked.concat(@rcd_result.hierarchy)
-    @hier_with_unranked << @rcd_result.unranked unless @rcd_result.unranked.empty?
+    hier_with_unranked = Hierarchy.new
+    hier_with_unranked.concat(rcd_result.hierarchy)
+    hier_with_unranked << rcd_result.unranked unless rcd_result.unranked.empty?
     # Create a flat list of the constraints in sorted order
-    sorted_cons = @hier_with_unranked.flatten
+    sorted_cons = hier_with_unranked.flatten
     # sort the ercs with respect to the RCD constraint hierarchy
-    sorted_ercs, @ercs_by_stratum, @explained_ercs = RCD_image.sort_rcd_results(@rcd_result)
+    sorted_ercs, ercs_by_stratum, explained_ercs = sort_rcd_results(rcd_result)
     return sorted_ercs, sorted_cons
   end
   protected :construct_ercs_and_constraints
@@ -50,7 +45,7 @@ class RCD_image < CT_image
   # Sort the ercs of an RCD result in several ways.
   #
   # :call-seq:
-  #   RCD_image.sort_rcd_results(rcd_result) -> [sorted_ercs, ercs_by_stratum, explained_ercs]
+  #   sort_rcd_results(rcd_result) -> [sorted_ercs, ercs_by_stratum, explained_ercs]
   #
   # The returned array contains three objects, each a list of ercs.
   # sorted_ercs:: all of the ercs in a flat array, sorted with respect to the
@@ -63,7 +58,7 @@ class RCD_image < CT_image
   # explained_ercs:: only the explained ercs (those that are properly accounted
   #                  for by RCD), in a flat array, sorted with respect to the
   #                  constraint hierarchy constructed by RCD.
-  def RCD_image.sort_rcd_results(rcd_result)
+  def sort_rcd_results(rcd_result)
     # Sort each "stratum" of ercs by the order of the constraints in the
     # corresponding constraint hierarchy stratum, so that the ercs assigned
     # a W by the first constraint in the stratum appear first, etc.
@@ -71,14 +66,15 @@ class RCD_image < CT_image
     flat_hier.concat(rcd_result.unranked) unless rcd_result.unranked.empty?
     ercs_by_stratum = []
     rcd_result.ex_ercs.each do |ercs|
-      ercs_by_stratum << RCD_image.sort_by_constraint_order(ercs,flat_hier)
+      ercs_by_stratum << ERCs_image.sort_by_constraint_order(ercs,flat_hier)
     end
     explained_ercs = ercs_by_stratum.flatten # in sorted order
 
     # add any unexplained ercs as the last level of the stratified erc list.
     unex_ercs = []
     unless rcd_result.unex_ercs.empty? then
-      unex_ercs = RCD_image.sort_by_constraint_order(rcd_result.unex_ercs, flat_hier)
+      unex_ercs =
+        ERCs_image.sort_by_constraint_order(rcd_result.unex_ercs, flat_hier)
       ercs_by_stratum << unex_ercs
     end
 
