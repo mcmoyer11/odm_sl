22f636b0c2509c9704ec04c6cd70aa24690788f0
Bruce Tesar <tesar@rutgers.edu>
2018-06-29 21:05:04 -0400

    Major refactor/simplification of RcdImage#sort_rcd_results.

--- lib/rcd_image.rb
+++ lib/rcd_image.rb
@@ -3,7 +3,6 @@
 
 require_relative "comparative_tableau_image"
 require_relative "sheet"
-require_relative "hierarchy"
 
 # A 2-dimensional sheet representation of an Rcd object, i.e.,
 # a comparative tableau of the ercs, typically winner-loser pairs, to which
@@ -60,50 +59,19 @@ class RcdImage
     # Create a flat list of the constraints in sorted order
     sorted_cons = hier_with_unranked.flatten
     # sort the ercs with respect to the RCD constraint hierarchy
-    sorted_ercs, ercs_by_stratum, explained_ercs = sort_rcd_results(rcd_result)
+    sorted_ercs = sort_rcd_results(rcd_result)
     return sorted_ercs, sorted_cons
   end
   protected :construct_ercs_and_constraints
 
-  # Sort the ercs of an RCD result in several ways.
-  #
-  # :call-seq:
-  #   sort_rcd_results(rcd_result) -> [sorted_ercs, ercs_by_stratum, explained_ercs]
-  #
-  # The returned array contains three objects, each a list of ercs.
-  # sorted_ercs:: all of the ercs in a flat array, sorted with respect to the
-  #               constraint hierarchy constructed by RCD.
-  # ercs_by_stratum:: all of the ercs in an array of strata, with each
-  #                   stratum corresponding to a stratum of the hierarchy
-  #                   constructed by RCD. The first erc stratum contains those
-  #                   ercs assigned a W by a constraint in the first stratum
-  #                   of the hierarchy, and so forth.
-  # explained_ercs:: only the explained ercs (those that are properly accounted
-  #                  for by RCD), in a flat array, sorted with respect to the
-  #                  constraint hierarchy constructed by RCD.
+  # Sort the ercs of an RCD result.
   def sort_rcd_results(rcd_result)
-    # Sort each "stratum" of ercs by the order of the constraints in the
-    # corresponding constraint hierarchy stratum, so that the ercs assigned
-    # a W by the first constraint in the stratum appear first, etc.
     flat_hier = rcd_result.hierarchy.flatten
     flat_hier.concat(rcd_result.unranked) unless rcd_result.unranked.empty?
-    ercs_by_stratum = []
-    rcd_result.ex_ercs.each do |ercs|
-      ercs_by_stratum << sort_by_constraint_order(ercs,flat_hier)
-    end
-    explained_ercs = ercs_by_stratum.flatten # in sorted order
-    # add any unexplained ercs as the last level of the stratified erc list.
-    unex_ercs = []
-    unless rcd_result.unex_ercs.empty? then
-      unex_ercs =
-        sort_by_constraint_order(rcd_result.unex_ercs, flat_hier)
-      ercs_by_stratum << unex_ercs
-    end
-    # create a list of the ercs in sorted order
-    sorted_ercs = Erc_list.new
-    sorted_ercs.add_all(explained_ercs)
-    sorted_ercs.add_all(unex_ercs)
-    return sorted_ercs, ercs_by_stratum, explained_ercs
+    erc_list = rcd_result.ex_ercs.flatten
+    erc_list << rcd_result.unex_ercs unless rcd_result.unex_ercs.empty?
+    sorted_ercs = sort_by_constraint_order(erc_list,flat_hier)
+    return sorted_ercs
   end
   protected :sort_rcd_results
 
@@ -113,6 +81,8 @@ class RcdImage
   # constraint, followed by all the ercs assigned an L by the first constraint.
   # Each of those blocks of ercs is recursively sorted by the other constraints
   # in order.
+  # 
+  # Returns a sorted array of ercs.
   #
   # This is used for display purposes, to create a monotonic "W boundary" in
   # formatted comparative tableaux.
