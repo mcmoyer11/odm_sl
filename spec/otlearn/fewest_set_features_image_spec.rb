e96ab4468e1757e383bacdb762f980d6cd1b8c79
Bruce Tesar <tesar@rutgers.edu>
2018-07-12 21:00:20 -0400

    Updated the specs for FewestSetFeaturesImage to reflect that info on the chosen failed loser is included regardless of whether any new information was obtained or not.

--- spec/otlearn/fewest_set_features_image_spec.rb
+++ spec/otlearn/fewest_set_features_image_spec.rb
@@ -4,17 +4,19 @@ require 'otlearn/fewest_set_features_image'
 
 RSpec.describe OTLearn::FewestSetFeaturesImage do
   let(:fsf_step){double('fsf_step')}
+  let(:failed_winner){double('failed_winner')}
+  let(:failed_winner_morphword){'failed_winner_morphword'}
+  let(:failed_winner_input){'failed_winner_input'}
+  let(:failed_winner_output){'failed_winner_output'}
+  before(:each) do
+    allow(fsf_step).to receive(:failed_winner).and_return(failed_winner)
+    allow(failed_winner).to receive(:morphword).and_return(failed_winner_morphword)
+    allow(failed_winner).to receive(:input).and_return(failed_winner_input)
+    allow(failed_winner).to receive(:output).and_return(failed_winner_output)    
+  end
   context "given a step with a newly set feature" do
-    let(:failed_winner){double('failed_winner')}
-    let(:failed_winner_morphword){'failed_winner_morphword'}
-    let(:failed_winner_input){'failed_winner_input'}
-    let(:failed_winner_output){'failed_winner_output'}
     before(:each) do
       allow(fsf_step).to receive(:changed?).and_return(true)
-      allow(fsf_step).to receive(:failed_winner).and_return(failed_winner)
-      allow(failed_winner).to receive(:morphword).and_return(failed_winner_morphword)
-      allow(failed_winner).to receive(:input).and_return(failed_winner_input)
-      allow(failed_winner).to receive(:output).and_return(failed_winner_output)
       @fsf_image = OTLearn::FewestSetFeaturesImage.new(fsf_step)
     end
     it "indicates the type of substep" do
@@ -35,7 +37,6 @@ RSpec.describe OTLearn::FewestSetFeaturesImage do
   end
 
   context "given a step without a newly set feature" do
-    let(:failed_winner){double('failed_winner')}
     before(:each) do
       allow(fsf_step).to receive(:changed?).and_return(false)
       @fsf_image = OTLearn::FewestSetFeaturesImage.new(fsf_step)
@@ -46,8 +47,14 @@ RSpec.describe OTLearn::FewestSetFeaturesImage do
     it "indicates the FSF did not change the grammar" do
       expect(@fsf_image[2,1]).to eq "Grammar Changed: FALSE"
     end
-    it "only contains two rows" do
-      expect(@fsf_image.row_count).to eq 2
+    it "indicates the morphword of the failed winner used" do
+      expect(@fsf_image[3,3]).to eq failed_winner_morphword
+    end
+    it "indicates the input of the failed winner used" do
+      expect(@fsf_image[3,4]).to eq failed_winner_input
+    end
+    it "indicates the output of the failed winner used" do
+      expect(@fsf_image[3,5]).to eq failed_winner_output
     end
   end
 
