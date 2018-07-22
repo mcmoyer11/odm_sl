8e805ecbb381a393de83a34f3f47752d6898f587
Bruce Tesar <tesar@rutgers.edu>
2018-07-21 23:11:01 -0400

    Eliminated the OTLearn.ranking_learning_faith_* methods; all have been replaced by OTLearn.ranking_learning().

--- lib/otlearn/ranking_learning.rb
+++ lib/otlearn/ranking_learning.rb
@@ -8,51 +8,20 @@ require_relative 'mrcd'
 
 module OTLearn
   
+  # Performs ranking learning on the +word list+, using the
+  # +grammar+. Loser selection is done via the +selector+.
+  # The +grammar+ is directly updated with the additional winner-loser
+  # pairs obtained. The class of object used to implement multi-recursive
+  # constraint demotion is optionally passed via the named parameter
+  # +mrcd_class+ (defaults to class Mrcd).
+  # 
+  # Returns the mrcd_class object representing the results of ranking learning.
   def OTLearn.ranking_learning(word_list, grammar, selector, mrcd_class: Mrcd)
     mrcd_result = mrcd_class.new(word_list, grammar, selector)
     mrcd_result.added_pairs.each { |pair| grammar.add_erc(pair) }
     return mrcd_result
   end
   
-  # Performs ranking learning on the given word list, using the
-  # given grammar. The parameter +grammar+ is directly updated
-  # with the additional winner-loser pairs obtained. Loser selection is
-  # done using the Faith Low ranking bias.
-  # 
-  # Returns true if any change at all is made to
-  # the grammar (any new winner-loser pairs are added).
-  # 
-  # The underlying form for each word is set so that each feature matches
-  # the underlying value if it is set in the lexicon, and otherwise
-  # matches the surface value of the feature in the word.
-  def OTLearn::ranking_learning_faith_low(word_list, grammar)
-    winner_list = data_dup_and_match_output(word_list)
-    # Use the faith-low ranking bias for ranking learning
-    selector = LoserSelector_by_ranking.new(grammar.system, rcd_class: OTLearn::RcdFaithLow)
-    mrcd_result = Mrcd.new(winner_list, grammar, selector)
-    mrcd_result.added_pairs.each { |pair| grammar.add_erc(pair) }
-    return mrcd_result.any_change?
-  end
-  
-  #returns an mrcd_result object rather than a boolean
-  def OTLearn::ranking_learning_faith_low_mrcd(word_list, grammar)
-    winner_list = data_dup_and_match_output(word_list)
-    # Use the faith-low ranking bias for ranking learning
-    selector = LoserSelector_by_ranking.new(grammar.system, rcd_class: OTLearn::RcdFaithLow)
-    mrcd_result = Mrcd.new(winner_list, grammar, selector)
-    mrcd_result.added_pairs.each { |pair| grammar.add_erc(pair) }
-    return mrcd_result
-  end
-  
-  def OTLearn::ranking_learning_faith_low_no_mod(word_list, grammar)
-    winner_list = word_list
-    # Use the faith-low ranking bias for ranking learning
-    selector = LoserSelector_by_ranking.new(grammar.system, rcd_class: OTLearn::RcdFaithLow)
-    mrcd_result = Mrcd.new(winner_list, grammar, selector)
-    mrcd_result.added_pairs.each { |pair| grammar.add_erc(pair) }
-    return mrcd_result
-  end
-
   # Performs ranking learning on the given word list, using the
   # given grammar. The parameter +grammar+ is directly updated
   # with the additional winner-loser pairs obtained. Loser selection is
