f7e870b3912948e8e443ee10c1a8275badfb787d
Bruce Tesar <tesar@rutgers.edu>
2018-07-25 22:31:04 -0400

    Removed a number of unused methods from Competition_list.

--- lib/competition_list.rb
+++ lib/competition_list.rb
@@ -1,5 +1,4 @@
 # Author: Bruce Tesar
-#
 
 # Stores a list of competitions comprising a dataset. 
 class Competition_list < Array
@@ -15,37 +14,6 @@ class Competition_list < Array
   def constraint_list
     return [] if empty?
     return self[0].constraint_list    
-  end
-
-  # Searches for duplicate inputs among the competitions in the list.
-  # Returns an array containing each input that appears in more than
-  # one competition. Returns an empty array if there are no duplicate inputs.
-  def find_duplicate_inputs
-    inputs = {}  # hash mapping each input to a list of the competitions containing it.
-    self.each{|c| (inputs[c.input] ||= []) << c}
-    dup_pairs = inputs.find_all{|input, list| list.size > 1}
-    return dup_pairs.map{|p| p[0]}
-  end
-
-  # Returns true if every competition in the list has exactly one
-  # candidate labeled as optimal; false otherwise.
-  def single_opt_per_competition?
-    return self.all?{|c| c.sing_optimum?}
-  end
-
-  # Calls Competition#merge_identical_candidates!() for each competition in
-  # the list, thus merging (within each competition) candidates with
-  # identical violation profiles.
-  def merge_identical_candidates!
-    self.each{|comp| comp.merge_identical_candidates!}
-  end
-
-  # Returns a competition list where, within each competition, candidates
-  # with identical violation profiles have been merged.
-  def merge_identical_candidates
-    new_comp_list = Competition_list.new(self.label)
-    merged_competitions = self.map{|comp| comp.merge_identical_candidates}
-    return new_comp_list.concat(merged_competitions)
   end
 
 end # class Competition_list
