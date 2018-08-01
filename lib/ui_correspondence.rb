aa46105ec83e2db14ff2a64cdd44e4e65ee02359
Bruce Tesar <tesar@rutgers.edu>
2018-08-01 01:04:54 -0400

    Converted UICorrespondence from a subclass of Array to a class with an internal array.

--- lib/ui_correspondence.rb
+++ lib/ui_correspondence.rb
@@ -1,13 +1,20 @@
 # Author: Bruce Tesar
-#
 
-# A UI correspondence relation contains underlying-input correspondence
-# pairs, each pair a size 2 array with the first element the underlying
+# A UI correspondence relates corresponding underlying-input elements.
+# The underlying elements belong to the lexical entries of the morphemes
+# for the input.
+# 
+# NOTE: a UI correspondence is specific to a particular instance of
+# a particular input. References to the actual
+# elements of the underlying form and input are stored in the correspondence,
+# and retrieval is based on object identity, using .equal?(), NOT ==.
+# This is important: two phonologically identical elements could have
+# separate existence in the same input (and even belong to
+# the same morpheme).
+#---
+# Each pair is a size 2 array with the first element the underlying
 # correspondent and the second element the input correspondent.
-# A separate UICorrespondence exists for every input; it realizes
-# the relation between the input and the lexical entries of its morphemes,
-# at the level of individual correspondence elements.
-class UICorrespondence < Array
+class UICorrespondence
 
   # The index in a correspondence pair for the underlying element.
   UF = 0
@@ -17,7 +24,7 @@ class UICorrespondence < Array
   
   # Returns an empty UICorrespondence.
   def initialize
-#    @pair_list = []
+    @pair_list = []
   end
 
   # Adds a correspondence pair indicating that +uf_el+ and +in_el+
@@ -26,54 +33,56 @@ class UICorrespondence < Array
     pair = []
     pair[UF] = uf_el
     pair[IN] = in_el
-    self << pair
+    @pair_list << pair
     return self
   end
   
-#  def size
-#    super.size
-#  end
-#  
-#  def [](idx)
-#    super[idx]
-#  end
-  
-  # Returns true if underlying element _under_ has an input
-  # correspondent. Returns false otherwise.
-  def in_corr?(under)
-    any?{|pair| pair[0].equal?(under)}    
+  # TODO: modernize the system_spec.rb files, so this method can be eliminated.
+  def size
+    @pair_list.size
   end
   
-  # Returns true if input element _input_ has an underlying
+  # TODO: modernize the system_spec.rb files, so this method can be eliminated.
+  def [](idx)
+    @pair_list[idx]
+  end
+  
+  # Returns true if underlying element +uf_el+ has an input
+  # correspondent. Returns false otherwise.
+  def in_corr?(uf_el)
+    @pair_list.any?{|pair| pair[UF].equal?(uf_el)}    
+  end
+  
+  # Returns true if input element +in_el+ has an underlying
   # correspondent (in the lexicon). Returns false otherwise.
-  def under_corr?(input)
-    any?{|pair| pair[1].equal?(input)}
+  def under_corr?(in_el)
+    @pair_list.any?{|pair| pair[IN].equal?(in_el)}
   end
 
-  # Returns the input correspondent for underlying element _under_.
-  # If _under_ has no input correspondent, then nil is returned.
+  # Returns the input correspondent for underlying element +uf_el+.
+  # If +uf_el+ has no input correspondent, then nil is returned.
   # 
-  # If _under_ has more than one correspondent, the first one listed
+  # If +uf_el+ has more than one correspondent, the first one listed
   # in the correspondence relation (unpredictable) is returned. *Note*:
   # if multiple correspondence is allowed, a different implementation
   # of the correspondence relation should be used.
-  def in_corr(under)
-    first_pair = find{|pair| pair[0].equal?(under)}
+  def in_corr(uf_el)
+    first_pair = @pair_list.find{|pair| pair[UF].equal?(uf_el)}
     return nil if first_pair.nil?
-    return first_pair[1]
+    return first_pair[IN]
   end
   
-  # Returns the underlying correspondent for input element _input_.
-  # If _input_ has no underlying correspondent, then nil is returned.
+  # Returns the underlying correspondent for input element +in_el+.
+  # If +in_el+ has no underlying correspondent, then nil is returned.
   #
-  # If _input_ has more than one correspondent, the first one listed
+  # If +in_el+ has more than one correspondent, the first one listed
   # in the correspondence relation (unpredictable) is returned. *Note*:
   # if multiple correspondence is allowed, a different implementation
   # of the correspondence relation should be used.
-  def under_corr(input)
-    first_pair = find{|pair| pair[1].equal?(input)}
+  def under_corr(in_el)
+    first_pair = @pair_list.find{|pair| pair[IN].equal?(in_el)}
     return nil if first_pair.nil?
-    return first_pair[0]
+    return first_pair[UF]
   end
 
 end # class UICorrespondence
