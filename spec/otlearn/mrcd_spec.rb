a8c7fb233b63807dba86701fa0af9ddbe8157e1f
Bruce Tesar <tesar@rutgers.edu>
2020-04-07 02:39:12 -0400

    Eliminated dependency on SL::Grammar in specs for MRCD.

--- spec/otlearn/mrcd_spec.rb
+++ spec/otlearn/mrcd_spec.rb
@@ -2,26 +2,33 @@
 
 require_relative '../../lib/otlearn/mrcd'
 require_relative '../../lib/loserselector_by_ranking'
-require_relative '../../lib/sl/grammar'
 
 RSpec.describe "MRCD" do
   context "with default rcd class and an empty word list" do
+    let(:sys){double('system')}
+    let(:grammar){double('grammar')}
+    let(:dup_grammar){double('dup_grammar')}
     before(:each) do
       @word_list = []
-      @grammar = SL::Grammar.new
-      selector = LoserSelector_by_ranking.new(@grammar.system)
-      @mrcd = OTLearn::Mrcd.new(@word_list, @grammar, selector)
+      allow(grammar).to receive(:dup_same_lexicon).and_return(dup_grammar)
+      allow(dup_grammar).to receive(:consistent?).and_return(true)
+      selector = LoserSelector_by_ranking.new(sys)
+      @mrcd = OTLearn::Mrcd.new(@word_list, grammar, selector)
     end
     it "should not have any changes to the ranking information" do
       expect(@mrcd.any_change?).not_to be true
     end
   end
   context "with rcd class faith_low and an empty word list" do
+    let(:sys){double('system')}
+    let(:grammar){double('grammar')}
+    let(:dup_grammar){double('dup_grammar')}
     before(:each) do
       @word_list = []
-      @grammar = SL::Grammar.new
-      selector = LoserSelector_by_ranking.new(@grammar.system, rcd_class: OTLearn::RcdFaithLow)
-      @mrcd = OTLearn::Mrcd.new(@word_list, @grammar, selector)
+      allow(grammar).to receive(:dup_same_lexicon).and_return(dup_grammar)
+      allow(dup_grammar).to receive(:consistent?).and_return(true)
+      selector = LoserSelector_by_ranking.new(sys, rcd_class: OTLearn::RcdFaithLow)
+      @mrcd = OTLearn::Mrcd.new(@word_list, grammar, selector)
     end
     it "should not have any changes to the ranking information" do
       expect(@mrcd.any_change?).not_to be true
