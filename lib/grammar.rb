5649291d0df00824ab1f7e03a2efd897e7208cec
Bruce Tesar <tesar@rutgers.edu>
2020-04-07 02:10:44 -0400

    Created a general class Grammar, to ultimately replace the specific Grammar classes in SL and PAS.

---
+++ lib/grammar.rb
@@ -0,0 +1,82 @@
+# Author: Bruce Tesar
+#
+
+require_relative 'erc_list'
+require_relative 'lexicon'
+
+# A grammar consists of a reference to a linguistic system,
+# a list of ERCs, and a lexicon.
+class Grammar
+  # The optional label assigned to the grammar.
+  attr_accessor :label
+
+  # The list of ercs defining the ranking information of the grammar.
+  attr_reader :erc_list
+
+  # The lexicon for the grammar.
+  attr_reader :lexicon
+
+  # The linguistic system associated with this grammar.
+  attr_reader :system
+
+  # :call-seq:
+  #   Grammar.new(system: mysystem) -> Grammar
+  #   Grammar.new(system: mysystem, erc_list: mylist, lexicon: mylexicon) -> Grammar
+  # 
+  # The first form returns a grammar with an empty ERC list and an empty lexicon.
+  # The second form returns a grammar with ERC list +mylist+ and lexicon +mylexicon+.
+  #
+  # The system parameter is mandatory.
+  # Raises an exception if no system parameter is provided.
+  def initialize(system: nil, erc_list: nil, lexicon: Lexicon.new)
+    if system.nil? then
+      raise "Grammar.new must be given a system parameter."
+    end
+    @system = system
+    @erc_list = erc_list
+    @erc_list ||= Erc_list.new(constraint_list: @system.constraints)
+    self.label = "Grammar"
+    @lexicon = lexicon
+  end
+
+  # Adds an erc to the list.
+  # Returns a reference to self (the grammar).
+  def add_erc(erc)
+    erc_list.add(erc)
+    return self
+  end
+
+  # Returns true if the ERC list is currently consistent; false otherwise.
+  def consistent?
+    erc_list.consistent?
+  end
+
+  # Returns a deep copy of the grammar, with a duplicates of the lexicon.
+  # The duplicate of the lexicon contains duplicates of the lexical entries,
+  # and the duplicate lexical entries contain duplicates of the underlying
+  # forms but references to the very same morpheme objects.
+  def dup
+    return self.class.new(erc_list: erc_list.dup, lexicon: lexicon.dup, system: system)
+  end
+
+  # Returns a copy of the grammar, with a copy of the ERC list, and
+  # a reference to the very same lexicon object.
+  def dup_same_lexicon
+    return self.class.new(erc_list: erc_list.dup, lexicon: lexicon, system: system)
+  end
+
+  # Returns the underlying form for the given morpheme, as stored in
+  # the grammar's lexicon. Returns nil if the morpheme does not appear
+  # in the lexicon.
+  def get_uf(morph)
+    @lexicon.get_uf(morph)
+  end
+
+  # Parses the given output, returning the full word (with input,
+  # correspondence). The input features match the lexicon for set features,
+  # and are unset in the input for features unset in the lexicon.
+  def parse_output(out)
+    system.parse_output(out, lexicon)
+  end
+
+end # class Grammar
---
+++ spec/grammar_spec.rb
@@ -0,0 +1,102 @@
+# Author: Bruce Tesar
+
+require_relative '../lib/grammar'
+
+RSpec.describe Grammar do
+  context "Grammar.new, when no system specified," do
+    it "raises an exception" do
+      expect{Grammar.new()}.to raise_exception("Grammar.new must be given a system parameter.")
+    end
+  end
+  
+  context "A new Grammar, with only the system specified," do
+    let(:sys){double('system')}
+    let(:morph){double('morph')}
+    before(:example) do
+      allow(sys).to receive(:constraints).and_return([])
+      @grammar = Grammar.new(system: sys)
+    end
+    it "returns a reference to system" do
+      expect(@grammar.system).to eq(sys)
+    end
+    it "returns an empty ERC list" do
+      expect(@grammar.erc_list).to be_empty
+    end
+    it "returns an empty lexicon" do
+      expect(@grammar.lexicon.size).to eq(0)
+    end
+    it "returns the label Grammar" do
+      expect(@grammar.label).to eq "Grammar"
+    end
+    it "returns nil when a lexical entry is requested" do
+      expect(@grammar.get_uf(morph)).to eq(nil)
+    end
+  end
+  
+  context "A new grammar, when given a system and a lexicon," do
+    let(:sys){double('system')}
+    let(:lex){instance_double(Lexicon)}
+    before(:example) do
+      allow(sys).to receive(:constraints).and_return([])
+      allow(lex).to receive(:get_uf).with("the_morph").and_return("the_uf")
+      @grammar = Grammar.new(system: sys, lexicon: lex)
+    end
+    it "returns the given lexicon" do
+      expect(@grammar.lexicon).to eq(lex)
+    end
+    it 'returns uf "the_uf" for the morpheme "the_morph"' do
+      expect(@grammar.get_uf("the_morph")).to eq("the_uf")
+    end
+  end
+  
+  context "A grammar" do
+    let(:sys){double('system')}
+    before(:example) do
+      allow(sys).to receive(:constraints).and_return([])
+      @grammar = Grammar.new(system: sys)
+    end
+    context "when duplicated with dup" do
+      before(:example) do
+        @dup = @grammar.dup
+      end
+      it "should have distinct objects for the ERC list" do
+        expect(@grammar.erc_list).not_to equal(@dup.erc_list)
+      end
+      it "should have distinct objects for lexicon" do
+        expect(@grammar.lexicon).not_to equal(@dup.lexicon)
+      end
+    end
+    context "when duplicated with dup_same_lexicon" do
+      before(:example) do
+        @dup = @grammar.dup_same_lexicon
+      end
+      it "should have distinct objects for the ERC list" do
+        expect(@grammar.erc_list).not_to equal(@dup.erc_list)
+      end
+      it "should have the same object for lexicon" do
+        expect(@grammar.lexicon).to equal(@dup.lexicon)
+      end
+    end
+  end
+  
+  context "when parse_output is called" do
+    let(:erc_list){double('erc_list')}
+    let(:lexicon){double('lexicon')}
+    let(:sys){double('system')}
+    let(:output){double('output')}
+    let(:word){double('word')}
+    before(:example) do
+      allow(sys).to receive(:constraints).and_return([])
+      allow(sys).to receive(:parse_output).with(output,lexicon).
+        and_return(word)
+      @grammar = Grammar.new(system: sys, erc_list: erc_list, lexicon: lexicon)
+      @return_value = @grammar.parse_output(output)
+    end
+    it "calls system.parse_output" do
+      expect(sys).to have_received(:parse_output)
+    end
+    it "returns the full word" do
+      expect(@return_value).to eq word
+    end
+  end
+end # describe Grammar
