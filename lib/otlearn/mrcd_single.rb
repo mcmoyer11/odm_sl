4bdcca3af6b535568fc83682d9486547b9f68aa1
Bruce Tesar <tesar@rutgers.edu>
2020-04-14 01:56:52 -0400

    Implemented some basic specs for OTLearn::MrcdSingle, enough to allow a basic implementation of the class itself.

--- lib/otlearn/mrcd_single.rb
+++ lib/otlearn/mrcd_single.rb
@@ -1,5 +1,7 @@
 # Author: Bruce Tesar
 
+require_relative "../win_lose_pair"
+
 module OTLearn
   
   # An MrcdSingle object contains the results of applying
@@ -8,9 +10,49 @@ module OTLearn
   class MrcdSingle
     
     # Returns a new MrcdSingle object.
-    def initialize
-      
+    # wl_pair_class - dependency injection parameter for testing.
+    def initialize(winner, grammar, selector, wl_pair_class: Win_lose_pair)
+      @winner = winner
+      @grammar = grammar.dup_same_lexicon
+      @added_pairs = []
+      @selector = selector
+      @wl_pair_class = wl_pair_class
+      run_mrcd_single
     end
+    
+    # Returns the additional winner-loser pairs added to the grammar
+    # in order to make the winner optimal.
+    def added_pairs
+      return @added_pairs
+    end
+    
+    # Returns the winner that MrcdSingle attempted to make optimal.
+    def winner
+      return @winner
+    end
+    
+    # Returns true if the internal grammar is consistent, false otherwise.
+    def consistent?
+      return @grammar.consistent?
+    end
+    
+    # Runs MRCD on the winner, using the given grammar and loser selector.
+    def run_mrcd_single
+      loser = @selector.select_loser(@winner, @grammar.erc_list)
+      while !loser.nil? do
+        # Create a new WL pair.
+        new_pair = @wl_pair_class.new(@winner, loser)
+        new_pair.label = @winner.morphword.to_s
+        # Add the new pair to the list and the grammar.
+        @added_pairs << new_pair
+        @grammar.add_erc(new_pair)
+        # break out of the loop if the grammar is inconsistent
+        break unless @grammar.consistent?
+        loser = @selector.select_loser(@winner, @grammar.erc_list)
+      end
+      return true
+    end
+    protected :run_mrcd_single
     
   end # class MrcdSingle
   
--- spec/otlearn/mrcd_single_spec.rb
+++ spec/otlearn/mrcd_single_spec.rb
@@ -5,31 +5,74 @@ require_relative '../../lib/otlearn/mrcd_single'
 RSpec.describe OTLearn::MrcdSingle, :wip do
   let(:winner){double('winner')}
   let(:morphword){double('morphword')}
+  let(:param_grammar){double('parameter grammar')}
+  let(:grammar){double('grammar')}
+  let(:erc_list){double('ERC list')}
+  let(:selector){double('loser selector')}
+  let(:loser1){double('loser1')}
+  let(:pair_class){double('wl_pair_class')}
+  let(:wl_pair1){double('wl_pair1')}
   before(:each) do
+    allow(param_grammar).to receive(:dup_same_lexicon).and_return(grammar)
     allow(winner).to receive(:morphword).and_return(morphword)
     allow(morphword).to receive(:to_s).and_return("morphword_name")
+    allow(wl_pair1).to receive(:label=)
   end
   
   context "with a word that is already optimal" do
     before(:each) do
-      # allow().to
-      # @mrcd_single = OTLearn::MrcdSingle.new(winner, grammar, selector)
+      allow(grammar).to receive(:consistent?).and_return(true)
+      allow(grammar).to receive(:erc_list).and_return(erc_list)
+      allow(selector).to receive(:select_loser).and_return(nil)
+      @mrcd_single = OTLearn::MrcdSingle.new(winner, param_grammar, selector)
     end
-    it "duplicates the parameter grammar"
-    it "creates no added pairs"
-    it "has a grammar that is consistent"
-    it "calls selector once"
+    it "duplicates the parameter grammar" do
+      expect(param_grammar).to have_received(:dup_same_lexicon)
+    end
+    it "creates no added pairs" do
+      expect(@mrcd_single.added_pairs.empty?).to be true
+    end
+    it "has a grammar that is consistent" do
+      expect(@mrcd_single.consistent?).to be true
+    end
+    it "calls selector once" do
+      expect(selector).to have_received(:select_loser).with(winner,erc_list)
+    end
   end
   
   context "with a word requiring one pair" do
     before(:each) do
-      # allow().to
-      # @mrcd_single = OTLearn::MrcdSingle.new(winner, grammar, selector)
+      allow(grammar).to receive(:consistent?).and_return(true)
+      allow(grammar).to receive(:erc_list).and_return(erc_list)
+      allow(grammar).to receive(:add_erc)
+      allow(selector).to receive(:select_loser).and_return(loser1,nil)
+      allow(pair_class).to receive(:new).with(winner,loser1).and_return(wl_pair1)
+      @mrcd_single = OTLearn::MrcdSingle.new(winner, param_grammar, selector, wl_pair_class: pair_class)
     end
-    it "creates one added pair"
-    it "creates a pair with the winner and the first loser"
-    it "has a grammar that is consistent"
-    it "calls selector twice"
+    it "returns the winner" do
+      expect(@mrcd_single.winner).to eq winner
+    end
+    it "creates one new winner-loser pair" do
+      expect(pair_class).to have_received(:new).with(winner,loser1)
+    end
+    it "labels the wl pair with the morphword name" do
+      expect(wl_pair1).to have_received(:label=).with('morphword_name')
+    end
+    it "returns one added pair" do
+      expect(@mrcd_single.added_pairs.size).to eq 1
+    end
+    it "returns the created pair" do
+      expect(@mrcd_single.added_pairs[0]).to eq wl_pair1
+    end
+    it "adds the created pair to the grammar" do
+      expect(grammar).to have_received(:add_erc).with(wl_pair1)
+    end
+    it "has a grammar that is consistent" do
+      expect(@mrcd_single.consistent?).to be true
+    end
+    it "calls selector twice" do
+      expect(selector).to have_received(:select_loser).exactly(2).times
+    end
   end
   
   context "with a word requiring two pairs" do
@@ -46,13 +89,37 @@ RSpec.describe OTLearn::MrcdSingle, :wip do
   
   context "with a word reaching inconsistency after one pair" do
     before(:each) do
-      # allow().to
-      # @mrcd_single = OTLearn::MrcdSingle.new(winner, grammar, selector)
+      # consistency isn't checked until after a winner-loser pair is added
+      allow(grammar).to receive(:consistent?).and_return(false)
+      allow(grammar).to receive(:erc_list).and_return(erc_list)
+      allow(grammar).to receive(:add_erc)
+      allow(selector).to receive(:select_loser).and_return(loser1)
+      allow(pair_class).to \
+        receive(:new).with(winner,loser1).and_return(wl_pair1)
+      @mrcd_single = OTLearn::MrcdSingle.new(winner, param_grammar, selector,
+        wl_pair_class: pair_class)
     end
-    it "creates one added pair"
-    it "creates a pair with the winner and the first loser"
-    it "has a grammar that is inconsistent"
-    it "calls selector twice"
+    it "creates one new winner-loser pair" do
+      expect(pair_class).to have_received(:new).with(winner,loser1)
+    end
+    it "returns one added pair" do
+      expect(@mrcd_single.added_pairs.size).to eq 1
+    end
+    it "returns the created pair" do
+      expect(@mrcd_single.added_pairs[0]).to eq wl_pair1
+    end
+    it "adds the created pair to the grammar" do
+      expect(grammar).to have_received(:add_erc).with(wl_pair1)
+    end
+    it "has a grammar that is inconsistent" do
+      expect(@mrcd_single.consistent?).to be false
+    end
+    it "calls selector once" do
+      expect(selector).to have_received(:select_loser).exactly(1).time
+    end
+    it "calls selector with the winner and the initial erc list" do
+      expect(selector).to have_received(:select_loser).with(winner,erc_list)      
+    end
   end
   
 end # RSpec.describe MrcdSingle
