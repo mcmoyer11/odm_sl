a1fd44194e707acc40e039f600d17d6ffd3304a8
Bruce Tesar <tesar@rutgers.edu>
2020-04-24 01:26:33 -0400

    Added some additional specs for OTLearn::Mrcd.

--- spec/otlearn/mrcd_spec.rb
+++ spec/otlearn/mrcd_spec.rb
@@ -76,4 +76,85 @@ RSpec.describe "MRCD", :wip do
     end
   end
 
+  # If any changes occur on the first pass, it should make a second
+  # pass through all of the winners.
+  context "with 3 winners producing 2 new pairs" do
+    let(:winner1){double('winner1')}
+    let(:winner2){double('winner2')}
+    let(:winner3){double('winner3')}
+    let(:mrcd_single1){double('mrcd_single1')}
+    let(:mrcd_single2){double('mrcd_single2')}
+    let(:mrcd_single3){double('mrcd_single3')}
+    let(:mrcd_single4){double('mrcd_single4')}
+    let(:mrcd_single5){double('mrcd_single5')}
+    let(:mrcd_single6){double('mrcd_single6')}
+    let(:new_pair1){double('new WL pair 1')}
+    let(:new_pair2){double('new WL pair 2')}
+    before(:each) do
+      @word_list = [winner1,winner2,winner3]
+      allow(single_mrcd_class).to receive(:new).and_return(mrcd_single1,
+        mrcd_single2,mrcd_single3,mrcd_single4,mrcd_single5,mrcd_single6)
+      allow(mrcd_single1).to receive(:added_pairs).and_return([new_pair1])
+      allow(mrcd_single2).to receive(:added_pairs).and_return([])
+      allow(mrcd_single3).to receive(:added_pairs).and_return([new_pair2])
+      allow(mrcd_single4).to receive(:added_pairs).and_return([])
+      allow(mrcd_single5).to receive(:added_pairs).and_return([])
+      allow(mrcd_single6).to receive(:added_pairs).and_return([])
+      allow(dup_grammar).to receive(:consistent?).and_return(true)
+      allow(dup_grammar).to receive(:add_erc).with(new_pair1)
+      allow(dup_grammar).to receive(:add_erc).with(new_pair2)
+      @mrcd = OTLearn::Mrcd.new(@word_list, grammar, selector,
+        single_mrcd_class: single_mrcd_class)
+    end
+    it "has changes to the ranking information" do
+      expect(@mrcd.any_change?).to be true
+    end
+    it "has a consistent grammar" do
+      expect(@mrcd.grammar.consistent?).to be true
+    end
+    it "returns 2 new pairs" do
+      expect(@mrcd.added_pairs.size).to eq 2
+    end
+    # 6 mrcd objects, 3 for each pass through the word list (with 3 winners)
+    it "creates 6 mrcd_single objects" do
+      expect(single_mrcd_class).to have_received(:new).exactly(6).times
+    end
+  end
+
+  # When a winner leads to inconsistency, MRCD should halt immediately,
+  # without processing the rest of the winners, or making another pass.
+  context "with 3 winners, the second yielding inconsistency" do
+    let(:winner1){double('winner1')}
+    let(:winner2){double('winner2')}
+    let(:winner3){double('winner3')}
+    let(:mrcd_single1){double('mrcd_single1')}
+    let(:mrcd_single2){double('mrcd_single2')}
+    let(:new_pair1){double('new WL pair 1')}
+    before(:each) do
+      @word_list = [winner1,winner2,winner3]
+      allow(single_mrcd_class).to receive(:new).and_return(mrcd_single1,
+        mrcd_single2)
+      allow(mrcd_single1).to receive(:added_pairs).and_return([new_pair1])
+      allow(mrcd_single2).to receive(:added_pairs).and_return([])
+      allow(dup_grammar).to receive(:consistent?).and_return(true, false, false)
+      allow(dup_grammar).to receive(:add_erc).with(new_pair1)
+      @mrcd = OTLearn::Mrcd.new(@word_list, grammar, selector,
+        single_mrcd_class: single_mrcd_class)
+    end
+    it "has changes to the ranking information" do
+      expect(@mrcd.any_change?).to be true
+    end
+    it "has an inconsistent grammar" do
+      expect(@mrcd.grammar.consistent?).to be false
+    end
+    it "returns 1 new pair" do
+      expect(@mrcd.added_pairs.size).to eq 1
+    end
+    # 2 mrcd objects, for first two winners of one pass
+    # Verifies that MRCD terminates as soon as inconsistency occurs.
+    it "creates 2 mrcd_single objects" do
+      expect(single_mrcd_class).to have_received(:new).exactly(2).times
+    end
+  end
+
 end # describe Mrcd
