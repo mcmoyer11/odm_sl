dd05280051c72a20c868857d63b196732e53c62a
Bruce Tesar <tesar@rutgers.edu>
2020-04-25 04:38:43 -0400

    Updated the acceptance test fixtures for SL (reflecting advances in inductive learning); now all the SL acceptance tests pass. Commented out the acceptance tests for PAS for now (they take long, and the fixtures are outdated).

--- spec/acceptance/pas_learning_spec.rb
+++ spec/acceptance/pas_learning_spec.rb
@@ -3,25 +3,27 @@
 # This acceptance spec runs learning on all 62 PAS languages,
 # and checks the generated learning outputs against the test fixtures.
 
-RSpec.describe "Running ODL on PAS", :acceptance do
-  before(:context) do
-    project_dir = "C:/Users/Tesar/NetBeansProjects/odm_sl"
-    @pas_fixture_dir = File.join(project_dir,'test','fixtures','pas_learning')
-    @generated_dir = File.join(project_dir,'temp','pas_learning')
-    executable_dir = File.join('bin','pas')
-    load "#{executable_dir}/r1s1_typology_learning_mmr.rb"
-  end
-  
-  (1..62).each do |num|
-    context "on language L#{num}" do
-      before(:example) do
-        @success = system "diff #{@pas_fixture_dir}/LgL#{num}.csv #{@generated_dir}/LgL#{num}.csv"
-      end
-      
-      it "produces output that matches its test fixture" do
-        expect(@success).to be true
-      end
-    end
-  end
+# BBT: 4/25/2020 Limiting the acceptance testing to SL for now.
 
-end # RSpec.describe
+#RSpec.describe "Running ODL on PAS", :acceptance do
+#  before(:context) do
+#    project_dir = "C:/Users/Tesar/NetBeansProjects/odm_sl"
+#    @pas_fixture_dir = File.join(project_dir,'test','fixtures','pas_learning')
+#    @generated_dir = File.join(project_dir,'temp','pas_learning')
+#    executable_dir = File.join('bin','pas')
+#    load "#{executable_dir}/r1s1_typology_learning_mmr.rb"
+#  end
+#  
+#  (1..62).each do |num|
+#    context "on language L#{num}" do
+#      before(:example) do
+#        @success = system "diff #{@pas_fixture_dir}/LgL#{num}.csv #{@generated_dir}/LgL#{num}.csv"
+#      end
+#      
+#      it "produces output that matches its test fixture" do
+#        expect(@success).to be true
+#      end
+#    end
+#  end
+#
+#end # RSpec.describe
