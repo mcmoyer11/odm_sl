14d102a367a6934fb427d74e4a7e8c8eee3df35b
Bruce Tesar <tesar@rutgers.edu>
2020-04-28 14:17:04 -0400

    Simplified generate_typology_1r1s.rb, eliminating unnecessary global singleton function write_language_list_to_file.

--- bin/sl/generate_typology_1r1s.rb
+++ bin/sl/generate_typology_1r1s.rb
@@ -8,17 +8,6 @@ require_relative '../../lib/sl/data'
 require_relative '../../lib/factorial_typology'
 require_relative '../../lib/otlearn/data_manip'
 
-# Writes a list +lang_list+ of language data to file +data_file+.
-# Uses Marshal to write objects to file.
-def write_language_list_to_file(lang_list, data_file)
-  File.open(data_file, 'wb') do |f|
-    lang_list.each do |lang|
-      outputs = OTLearn::convert_wl_pairs_to_learning_data(lang)
-      Marshal.dump(["Lg#{lang.label}",outputs], f)
-    end
-  end  
-end
-
 # Generate the language typology data:
 # a list of sets of language data, one for each language in
 # the typology of the SL system, with each root and each suffix
@@ -27,8 +16,14 @@ competition_list = SL.generate_competitions_1r1s
 ft_result = FactorialTypology.new(competition_list)
 lang_list = ft_result.factorial_typology
 
-# Write the languages to a file
+# Write the data for each language of the typology to a data file.
+# Uses Marshal to write objects to file.
 data_path = File.join(File.dirname(__FILE__),'..','..','data','sl')
 Dir.mkdir(data_path) unless Dir.exists?(data_path)
 data_file = File.join(data_path,'outputs_typology_1r1s.mar')
-write_language_list_to_file(lang_list, data_file)
+File.open(data_file, 'wb') do |f|
+  lang_list.each do |lang|
+    outputs = OTLearn::convert_wl_pairs_to_learning_data(lang)
+    Marshal.dump(["Lg#{lang.label}",outputs], f)
+  end
+end  
