faa9492620b2c0d753f24c846294440bc83bd42b
Bruce Tesar <tesar@rutgers.edu>
2020-04-29 01:37:42 -0400

    Updated bin/main.rb to print an informative message when run.

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,12 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+# Author: Bruce Tesar
+#
+# The project's main file.
+# For now, it is just a placeholder, so if the main file is called,
+# an informative message is printed, rather than an error message.
 
-puts "Hello World"
+puts "bin/main.rb."
+puts "This is the main project file. For now, it is just a placeholder."
+puts "To actually do something, call one of the other, specialized scripts"
+puts "in bin/."
+puts "\nAt some point in the future, this main file might do something more"
+puts "meaningful, like start up a GUI interface for the project."
