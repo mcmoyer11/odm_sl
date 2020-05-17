ab618e2e067b8acb94fce2a4ee270508c88bac55
Bruce Tesar <tesar@rutgers.edu>
2020-05-17 13:12:44 -0400

    Updated generate_typology_1r1s.rb so that it checks for the existence of the /data folder, and creates it if necessary. This is useful the first time the script is run in a new clone of the project.

--- bin/sl/generate_typology_1r1s.rb
+++ bin/sl/generate_typology_1r1s.rb
@@ -15,14 +15,19 @@ competition_list = SL.generate_competitions_1r1s
 ft_result = FactorialTypology.new(competition_list)
 lang_list = ft_result.factorial_typology
 
+# Check for existence of data directories, and create them if necessary.
+data_path = File.join(File.dirname(__FILE__), '..', '..', 'data')
+Dir.mkdir(data_path) unless Dir.exists?(data_path)
+out_path = File.join(data_path, 'sl')
+Dir.mkdir(out_path) unless Dir.exists?(out_path)
+
+
 # Write the data for each language of the typology to a data file.
 # Uses Marshal to write objects to file.
-data_path = File.join(File.dirname(__FILE__),'..','..','data','sl')
-Dir.mkdir(data_path) unless Dir.exists?(data_path)
-data_file = File.join(data_path,'outputs_typology_1r1s.mar')
-File.open(data_file, 'wb') do |f|
+out_file = File.join(out_path, 'outputs_typology_1r1s.mar')
+File.open(out_file, 'wb') do |f|
   lang_list.each do |lang|
     outputs = OTLearn::convert_wl_pairs_to_learning_data(lang)
-    Marshal.dump(["Lg#{lang.label}",outputs], f)
+    Marshal.dump(["Lg#{lang.label}", outputs], f)
   end
-end  
+end
