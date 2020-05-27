2f78306ce0b6dab5a2b033f2f987c9d1ce1d049b
Bruce Tesar <tesar@rutgers.edu>
2020-05-27 18:14:28 -0400

    Converted SingleFormLearning to use LoserSelector based on CompareConsistency as the default for loser selection.

--- lib/otlearn/single_form_learning.rb
+++ lib/otlearn/single_form_learning.rb
@@ -1,43 +1,64 @@
+# frozen_string_literal: true
+
 # Author: Bruce Tesar
 
-require_relative "./grammar_test"
-require_relative "./data_manip"
-require_relative 'language_learning'
-require_relative 'ranking_learning'
+require 'otlearn/grammar_test'
+require 'otlearn/data_manip'
+require 'otlearn/language_learning'
+require 'otlearn/ranking_learning'
+require 'compare_consistency'
+require 'loser_selector'
+require 'loser_selector_from_gen'
 
 module OTLearn
-  
+
   # This processes all of the outputs in the grammatical output list, one at a
   # time in order, with respect to a grammar. Any results of learning are
   # realized as side effect changes to the grammar.
   class SingleFormLearning
     # The type of learning step
     attr_accessor :step_type
-    
+
+    # The list of outputs used for learning.
+    attr_reader :output_list
+
+    # The grammar resulting from this run of single form learning.
+    attr_reader :grammar
+
+    # Grammar test result after the completion of single form learning.
+    attr_reader :test_result
+
     # Creates the object, and automatically runs single form learning.
     # * +output_list+ - the list of all grammatical outputs.
     # * +grammar+ - the grammar that learning will modify.
+    # * +loser_selector+ - selects losers for ranking learning; defaults to
+    #   a loser selector using CompareConsistency.
+    #--
+    # learning_module and grammar_test_class are dependency injections used
+    # for testing.
     # * +learning_module+ - the module containing several methods used
     #   for learning: #ranking_learning, #mismatch_consistency_check,
     #   #set_uf_values, and #new_rank_info_from_feature.
-    #   Used for testing (dependency injection).
-    # * +grammar_test_class+ - the class of object used for testing
-    #   words for learning errors. Used for testing (dependency injection).
+    # * +grammar_test_class+ - the class of the object used to test
+    #   the grammar.
+    #++
     #
     # :call-seq:
-    #   SingleFormLearning.new(output_list, grammar) -> obj
-    #   SingleFormLearning.new(output_list, grammar, learning_module: module, grammar_test_class: class, loser_selector: obj) -> obj
-    def initialize(output_list, grammar,
-        learning_module: OTLearn, grammar_test_class: OTLearn::GrammarTest,
-        loser_selector: nil)
+    #   SingleFormLearning.new(output_list, grammar) -> singleformlearner
+    #   SingleFormLearning.new(output_list, grammar, loser_selector: selector) -> singleformlearner
+    def initialize(output_list, grammar, loser_selector: nil,
+                   learning_module: OTLearn,
+                   grammar_test_class: OTLearn::GrammarTest)
       @output_list = output_list
       @grammar = grammar
       @learning_module = learning_module
       @grammar_test_class = grammar_test_class
       # Cannot put the default in the parameter list because of the call
       # to grammar.system.
-      if loser_selector.nil? then
-        @loser_selector = LoserSelectorExhaustive.new(grammar.system)
+      if loser_selector.nil?
+        basic_selector = LoserSelector.new(CompareConsistency.new)
+        @loser_selector = LoserSelectorFromGen.new(grammar.system,
+                                                   basic_selector)
       else
         @loser_selector = loser_selector
       end
@@ -45,26 +66,10 @@ module OTLearn
       @changed = false
       run_single_form_learning
     end
-    
-    # The list of outputs used for learning.
-    def output_list
-      @output_list
-    end
-    
-    # The grammar resulting from this run of single form learning.
-    def grammar
-      @grammar
-    end
 
     # Returns true if single form learning changed the grammar; false otherwise.
     def changed?
-      return @changed
-    end
-    
-    # Returns the results of a grammar test after the completion of
-    # single form learning.
-    def test_result
-      @test_result
+      @changed
     end
 
     # Returns true if all words are correctly processed by the grammar;
@@ -72,9 +77,9 @@ module OTLearn
     def all_correct?
       @test_result.all_correct?
     end
-    
+
     # Processes the observed outputs for new grammatical information.
-    # 
+    #
     # Passes repeatedly through the list of outputs until a pass is made
     # with no changes to the grammar. For each output:
     # * Parse the output into a winner candidate, with all input feature
@@ -84,7 +89,7 @@ module OTLearn
     #   surface values). If it is, don't bother
     #   processing it (there is no learning error).
     # * If a learning error is detected, process the winner for new information.
-    # 
+    #
     # A boolean is returned indicating if the grammar was changed at all
     # during the execution of this method.
     def run_single_form_learning
@@ -95,7 +100,7 @@ module OTLearn
           # optimum for the mismatched input.
           grammar_test = @grammar_test_class.new([output], @grammar)
           # Unless no error is detected, try learning with the winner.
-          unless grammar_test.all_correct? then
+          unless grammar_test.all_correct?
             grammar_changed_on_winner = process_winner(output)
             grammar_changed_on_pass = true if grammar_changed_on_winner
           end
@@ -103,7 +108,7 @@ module OTLearn
         @changed = true if grammar_changed_on_pass
       end while grammar_changed_on_pass
       @test_result = @grammar_test_class.new(@output_list, @grammar)
-      return changed?
+      changed?
     end
     protected :run_single_form_learning
 
@@ -131,12 +136,13 @@ module OTLearn
       winner.match_input_to_output!
       # Check the winner to see if it is the sole optimum for
       # the matched input; if not, more ranking info is gained.
-      # NOTE: several languages aren't learned if this step isn't taken, if
-      # loser selection by ranking is used; if exhaustive loser selection is
-      # used, this extra ranking info check doesn't seem to be needed.
-#      new_ranking_info =
-#        @learning_module.ranking_learning([winner], @grammar, @loser_selector)
-#      change_on_winner = true if new_ranking_info.any_change?
+      # TODO: several languages aren't learned if this step isn't taken, if
+      #       loser selection by ranking is used; if exhaustive loser selection
+      #       is used, this extra ranking info check doesn't seem to be needed.
+      # new_ranking_info =
+      #   @learning_module.ranking_learning([winner], @grammar, @loser_selector)
+      # change_on_winner = true if new_ranking_info.any_change?
+      # ===
       # Check the mismatched input for consistency. Only attempt to set
       # features in the winner if the mismatched winner is inconsistent.
       consistency_result =
@@ -152,15 +158,14 @@ module OTLearn
         # For each newly set feature, check words unfaithfully mapping that
         # feature for new ranking information.
         set_feature_list.each do |set_f|
-          @learning_module.
-            new_rank_info_from_feature(@grammar, winner_list, set_f,
-            loser_selector: @loser_selector)
+          @learning_module\
+            .new_rank_info_from_feature(@grammar, winner_list, set_f,
+                                        loser_selector: @loser_selector)
         end
         change_on_winner = true unless set_feature_list.empty?
       end
-      return change_on_winner
+      change_on_winner
     end
     protected :process_winner
-    
-  end # class SingleFormLearning
-end # module OTLearn
+  end
+end
--- spec/otlearn/single_form_learning_spec.rb
+++ spec/otlearn/single_form_learning_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Author: Bruce Tesar
 
 require 'otlearn/single_form_learning'
@@ -5,23 +7,23 @@ require 'otlearn/language_learning'
 require 'otlearn/grammar_test'
 
 RSpec.describe OTLearn::SingleFormLearning do
-  let(:win1){double('winner 1')}
-  let(:out1){double('output 1')}
-  let(:grammar){double('grammar')}
-  let(:grammar_test_class){double('grammar_test_class')}
-  let(:grammar_test){instance_double(OTLearn::GrammarTest)}
-  let(:otlearn_module){double('OTLearn module')}
-  let(:consistency_result){double('consistency_result')}
-  let(:cr_grammar){double('cr_grammar')}
-  let(:loser_selector){double('loser_selector')}
-  let(:mrcd_result){double('mrcd_result')}
+  let(:win1) { double('winner 1') }
+  let(:out1) { double('output 1') }
+  let(:grammar) { double('grammar') }
+  let(:grammar_test_class) { double('grammar_test_class') }
+  let(:grammar_test) { instance_double(OTLearn::GrammarTest) }
+  let(:otlearn_module) { double('OTLearn module') }
+  let(:consistency_result) { double('consistency_result') }
+  let(:cr_grammar) { double('cr_grammar') }
+  let(:loser_selector) { double('loser_selector') }
+  let(:mrcd_result) { double('mrcd_result') }
   before(:example) do
     allow(win1).to receive(:match_input_to_output!)
   end
-  
-  context "with one correct winner" do
-    let(:winner_list){[win1]}
-    let(:output_list){[out1]}
+
+  context 'with one correct winner' do
+    let(:winner_list) { [win1] }
+    let(:output_list) { [out1] }
     before(:example) do
       allow(output_list).to receive(:map).and_return(winner_list)
       allow(grammar).to receive(:parse_output).with(out1).and_return(win1)
@@ -34,42 +36,42 @@ RSpec.describe OTLearn::SingleFormLearning do
         grammar_test_class: grammar_test_class,
         loser_selector: loser_selector)
     end
-    it "does not change the grammar" do
+    it 'does not change the grammar' do
       expect(@single_form_learning).not_to be_changed
     end
-    it "returns an output list with one output" do
+    it 'returns an output list with one output' do
       expect(@single_form_learning.output_list).to eq output_list
     end
-    it "returns the grammar" do
+    it 'returns the grammar' do
       expect(@single_form_learning.grammar).to eq grammar
     end
-    it "tests the winner once during learning, all winners afterward" do
+    it 'tests the winner once during learning, all winners afterward' do
       expect(grammar_test_class).to have_received(:new).exactly(2).times
-    end    
-    it "does not perform a mismatch consistency check" do
+    end
+    it 'does not perform a mismatch consistency check' do
       expect(otlearn_module).not_to have_received(:mismatch_consistency_check)
     end
-    it "gives the grammar test result" do
+    it 'gives the grammar test result' do
       expect(@single_form_learning.test_result).to eq grammar_test
     end
-    it "indicates that all words are handled correctly" do
+    it 'indicates that all words are handled correctly' do
       expect(@single_form_learning.all_correct?).to be true
     end
-    it "has step type SINGLE_FORM" do
+    it 'has step type SINGLE_FORM' do
       expect(@single_form_learning.step_type).to \
         eq OTLearn::LanguageLearning::SINGLE_FORM
     end
   end
-  
-  context "with one incorrect winner with a settable feature and other unsettable features" do
-    let(:winner_list){[win1]}
-    let(:output_list){[out1]}
+
+  context 'with one incorrect winner with a settable feature and other unsettable features' do
+    let(:winner_list) { [win1] }
+    let(:output_list) { [out1] }
     before(:example) do
       allow(output_list).to receive(:map).and_return(winner_list)
       allow(grammar).to receive(:parse_output).with(out1).and_return(win1)
       allow(otlearn_module).to receive(:mismatch_consistency_check).and_return(consistency_result)
-      allow(otlearn_module).to receive(:set_uf_values).with([win1], grammar).and_return(["feature1"],[])
-      allow(otlearn_module).to receive(:new_rank_info_from_feature).with(grammar,winner_list,"feature1",loser_selector: loser_selector)
+      allow(otlearn_module).to receive(:set_uf_values).with([win1], grammar).and_return(['feature1'], [])
+      allow(otlearn_module).to receive(:new_rank_info_from_feature).with(grammar, winner_list, 'feature1', loser_selector: loser_selector)
       allow(otlearn_module).to receive(:ranking_learning).and_return(mrcd_result)
       allow(mrcd_result).to receive(:any_change?).and_return(false)
       allow(grammar_test_class).to receive(:new).and_return(grammar_test)
@@ -82,33 +84,32 @@ RSpec.describe OTLearn::SingleFormLearning do
         grammar_test_class: grammar_test_class,
         loser_selector: loser_selector)
     end
-    it "changes the grammar" do
+    it 'changes the grammar' do
       expect(@single_form_learning).to be_changed
     end
-    it "returns an output list with one output" do
+    it 'returns an output list with one output' do
       expect(@single_form_learning.output_list).to eq output_list
     end
-    it "returns the grammar" do
+    it 'returns the grammar' do
       expect(@single_form_learning.grammar).to eq grammar
     end
-    it "performs two mismatch consistency checks" do
-      expect(otlearn_module).to have_received(:mismatch_consistency_check).with(grammar,[win1]).exactly(2).times
+    it 'performs two mismatch consistency checks' do
+      expect(otlearn_module).to have_received(:mismatch_consistency_check).with(grammar, [win1]).exactly(2).times
     end
-    it "calls set_uf_features twice" do
-      expect(otlearn_module).to have_received(:set_uf_values).with([win1],grammar).exactly(2).times
+    it 'calls set_uf_features twice' do
+      expect(otlearn_module).to have_received(:set_uf_values).with([win1], grammar).exactly(2).times
     end
-    it "checks for new ranking information on the set feature once" do
-      expect(otlearn_module).to have_received(:new_rank_info_from_feature).with(grammar,winner_list,"feature1",loser_selector:loser_selector).exactly(1).times
+    it 'checks for new ranking information on the set feature once' do
+      expect(otlearn_module).to have_received(:new_rank_info_from_feature).with(grammar, winner_list, 'feature1', loser_selector:loser_selector).exactly(1).times
     end
-    it "tests the winner twice during learning, all winners afterward" do
+    it 'tests the winner twice during learning, all winners afterward' do
       expect(grammar_test_class).to have_received(:new).exactly(3).times
-    end    
-    it "gives the grammar test result" do
+    end
+    it 'gives the grammar test result' do
       expect(@single_form_learning.test_result).to eq grammar_test
     end
-    it "indicates that not all words are handled correctly" do
+    it 'indicates that not all words are handled correctly' do
       expect(@single_form_learning.all_correct?).to be false
     end
   end
-  
-end # RSpec.describe OTLearn::SingleFormLearning
+end
