d53f208df692a7c003a29ebac5897061b8b69e4e
Bruce Tesar <tesar@rutgers.edu>
2020-06-07 17:28:32 -0400

    Cleaned up code in CsvOutput, wrote a couple of specs for it.

--- lib/csv_output.rb
+++ lib/csv_output.rb
@@ -1,26 +1,32 @@
+# frozen_string_literal: true
+
 # Author: Bruce Tesar
 
-require_relative 'sheet'
-require_relative 'otlearn/grammar_test_image'
+require 'sheet'
 require 'csv'
 
 # Constructs a CSV (comma-separated value) representation of a
-# 2-dimensional page image. The image is passed as a parameter to 
-# the constructor. A separate method, #write_to_file, can be
-# used to write the CSV formatted output to a file.
-# Uses the CSV library.
+# 2-dimensional +page_image+. An instance method, #write_to_file, can be
+# used to write the CSV formatted output to a file. Uses the CSV library.
+#
+# :call-seq:
+#   CsvOutput.new(page_image) -> csv_output
+#--
+# The named parameters sheet and csv_class are dependency injections used
+# for testing.
 class CsvOutput
   # Takes a page image, and automatically constructs a CSV image of it.
-  def initialize(page_image)
+  def initialize(page_image, sheet: Sheet.new, csv_class: CSV)
     @page_image = page_image
-    @csv_image = Sheet.new
+    @csv_image = sheet
+    @csv_class = csv_class
     construct_csv_image
   end
-  
-  # Write the CSV-formatted image to the file _destination_.
+
+  # Write the CSV-formatted image to the file named _destination_.
   # It writes the column headers as the first line.
   def write_to_file(destination)
-    CSV.open(destination, "w", {:write_headers=>true}) do |csv|
+    @csv_class.open(destination, 'w', { write_headers: true }) do |csv|
       # Convert the csv image to an array, and send each row of the array
       # to CSV.
       @csv_image.to_a.each do |row|
@@ -28,25 +34,24 @@ class CsvOutput
       end
     end
   end
-  
+
   # Writes the page image to the csv image, leaving the first row
   # as a header row filled with blanks.
   def construct_csv_image
     # Put the image in row 2, leaving row 1 as a header row.
-    @csv_image.put_range[2,1] = @page_image
+    @csv_image.put_range[2, 1] = @page_image
     headers_nil_to_blank
   end
-  protected :construct_csv_image
-  
+  private :construct_csv_image
+
   # Pad the first row so that any empty cells contain a blank (not nil).
   # The first row is treated as a header row by the NetBeans CSV editor,
   # and if an entry is nil, the CSV editor ignores the entire column,
   # truncating the display in the editor.
   def headers_nil_to_blank
     (1..@csv_image.col_count).each do |col|
-      @csv_image[1,col]=" " unless @csv_image[1,col]
+      @csv_image[1, col] = ' ' unless @csv_image[1, col]
     end
   end
-  protected :headers_nil_to_blank
-  
-end # class CSV_Output
+  private :headers_nil_to_blank
+end
--- spec/csv_output_spec.rb
+++ spec/csv_output_spec.rb
@@ -3,13 +3,44 @@
 # Author: Bruce Tesar
 
 require 'rspec'
+require 'csv_output'
 
 RSpec.describe 'CsvOutput' do
+  let(:page) { double('page image') }
+  let(:sheet) { double('sheet') }
+  let(:csv) { double('CSV class') }
+  let(:inner_class) { double('inner class') }
   before(:example) do
-    # Do nothing
+    allow(sheet).to receive(:put_range).and_return(inner_class)
+    allow(inner_class).to receive(:[]=)
+    allow(sheet).to receive(:col_count).and_return(1)
+    allow(sheet).to receive(:[]=)
   end
 
-  context 'when condition' do
-    it 'succeeds'
+  context 'given a sheet with blanks in headers' do
+    before(:example) do
+      allow(sheet).to receive(:[]).and_return(' ')
+      @csv_output = CsvOutput.new(page, sheet: sheet, csv_class: csv)
+    end
+    it 'puts the page image on the sheet' do
+      expect(sheet).to have_received(:put_range)
+      expect(inner_class).to have_received(:[]=).with(2, 1, page)
+    end
+    it 'does not alter the headers' do
+      expect(sheet).not_to have_received(:[]=)
+    end
+  end
+  context 'given a sheet with nils in headers' do
+    before(:example) do
+      allow(sheet).to receive(:[]).and_return(nil)
+      @csv_output = CsvOutput.new(page, sheet: sheet, csv_class: csv)
+    end
+    it 'puts the page image on the sheet' do
+      expect(sheet).to have_received(:put_range)
+      expect(inner_class).to have_received(:[]=).with(2, 1, page)
+    end
+    it 'converts the headers to blanks' do
+      expect(sheet).to have_received(:[]=).with(1, 1, ' ')
+    end
   end
 end
