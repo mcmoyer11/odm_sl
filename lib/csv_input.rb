b2e8e21c7a584aa9733ec0d0679e6162d1fdbbe8
Bruce Tesar <tesar@rutgers.edu>
2020-06-06 23:43:51 -0400

    Renamed CSV_Input to proper camelcase CsvInput, and cleaned up the code for the class and specs.

--- lib/csv_input.rb
+++ lib/csv_input.rb
@@ -1,32 +1,31 @@
+# frozen_string_literal: true
+
 # Author: Bruce Tesar
 
 require 'csv'
 
-# A CSV_Input object is given a filename when constructed. It opens the file,
+# A CsvInput object is given a filename when constructed. It opens the file,
 # attempts to read the entire file as a CSV file, and produces an array
 # of +headers+ (the first row of the CSV file), and an array +data+ of
 # arrays (the other rows of the CSV file).
-class CSV_Input
+class CsvInput
+  # An array of the column headers (as strings).
+  attr_reader :headers
+
+  # The data rows (rows 2 and later) as an array of arrays,
+  # with each row array being an array of strings.
+  attr_reader :data
 
   # Reads the contents of a CSV file. +infilename+ is a string that is
   # the filename of the file to be read. The contents of the file are
-  # accessible from the returned CSV_Input object.
+  # accessible from the returned CsvInput object.
+  # :call-seq:
+  #   CsvInput.new(infilename) -> csv_input
   def initialize(infilename)
     @filename = infilename
     @headers = []
     @data = []
     read_from_file
-  end
-
-  # Returns an array of the column headers (as strings).
-  def headers
-    return @headers
-  end
-
-  # Returns the data rows (rows 2 and later) as an array of arrays,
-  # which each row array being an array of strings.
-  def data
-    return @data
   end
 
   # Protected method, invoked by the constructor, that reads the contents
@@ -36,5 +35,5 @@ class CSV_Input
     @data = CSV.read(@filename)
     @headers = @data.shift
   end
-  protected :read_from_file
-end # class CSV_Input
+  private :read_from_file
+end
--- spec/csv_input_spec.rb
+++ spec/csv_input_spec.rb
@@ -1,21 +1,24 @@
+# frozen_string_literal: true
+
 # Author: Bruce Tesar
 
 require 'csv_input'
 
-project_dir = File.absolute_path(File.join(File.dirname(__FILE__),'..'))
-fixture_dir = File.join(project_dir,'test','fixtures')
+project_dir = File.absolute_path(File.join(File.dirname(__FILE__), '..'))
+fixture_dir = File.join(project_dir, 'test', 'fixtures')
 
-RSpec.describe(CSV_Input, "A CSV_Input") do
-  context "when created with a valid CSV filename" do
+RSpec.describe 'CsvInput' do
+  context 'when created with a valid CSV filename' do
     before(:example) do
-      infile = File.join(fixture_dir,'erc_input1.csv')
-      @csv_input = CSV_Input.new(infile)
+      infile = File.join(fixture_dir, 'erc_input1.csv')
+      @csv_input = CsvInput.new(infile)
     end
-    it "returns an array of column headers" do
-      expect(@csv_input.headers).to eq(['ERC_Label','Con1','Con2','Con3'])
+    it 'returns an array of column headers' do
+      expect(@csv_input.headers).to eq(['ERC_Label', 'Con1', 'Con2', 'Con3'])
     end
-    it "returns an array of arrays of the data" do
-      expect(@csv_input.data).to eq([['E1','W','L','W'],['E2','e','W','L']])
+    it 'returns an array of arrays of the data' do
+      expect(@csv_input.data).to eq([['E1', 'W', 'L', 'W'],
+                                     ['E2', 'e', 'W', 'L']])
     end
   end
 end
