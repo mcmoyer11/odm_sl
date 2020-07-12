d2a6590677c8eb97fcf6a90a22a0c573b53bd240
Bruce Tesar <tesar@rutgers.edu>
2020-07-12 00:13:28 -0400

    Converted Feature from a base class to a mixin (module). It can now be mixed in to any concrete feature class and provide almost all the necessary functionality.

--- lib/feature.rb
+++ lib/feature.rb
@@ -2,38 +2,64 @@
 
 # Author: Bruce Tesar
 
-# This is the base class for features; subclasses will provide the specific
-# properties (like possible values) for specific feature types.
-class Feature
-  # The value of the feature.
-  attr_accessor :value
-
-  # The feature type.
-  attr_accessor :type
-
+# A mixin for feature classes. The host class must initialize
+# the attributes _type_, _value_, and _value_list_.
+module Feature
   # The feature value for an unset feature.
   UNSET = nil
 
-  # A feature type is set to _type_. The feature value is initialized
-  # to unset.
-  def initialize(type)
-    @value = UNSET
-    @type = type
+  # The feature type.
+  attr_reader :type
+
+  # A list of the possible feature values. Does NOT include unset.
+  attr_reader :value_list
+
+  # The value of the feature.
+  attr_accessor :value
+
+  # Inner module containing methods to be added as class methods.
+  module ClassMethods
+    def feature_value(name)
+      define_method("#{name}?") { value == name }
+      define_method("set_#{name}") do
+        self.value = name
+        self
+      end
+    end
+  end
+
+  # Hook method that is executed whenever this module is included into
+  # a class (the host class). Here, it uses _extend_ to add the contents
+  # of _ClassMethods_ as class methods to the host class.
+  def self.included(host_class)
+    host_class.extend(ClassMethods)
+  end
+
+  # Returns true if a feature is unset; false otherwise.
+  def unset?
+    value == UNSET
+  end
+
+  # Returns true if _val_ is a valid value for the feature;
+  # returns false otherwise.
+  def valid_value?(val)
+    value_list.include?(val)
+  end
+
+  # An iterator for possible values of the feature. It yields the possible
+  # values of the feature to the provided block.
+  def each_value
+    value_list.each { |val| yield val }
   end
 
   # Distinct feature objects are equivalent if they have
   # equivalent type and value.
   def ==(other)
-    (@type == other.type) && (@value == other.value)
+    (type == other.type) && (value == other.value)
   end
 
   # The same as ==(_other_).
   def eql?(other)
     self == other
-  end
-
-  # Returns true if a feature is unset; false otherwise.
-  def unset?
-    @value == UNSET
   end
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,69 +1,88 @@
+# frozen_string_literal: true
+
 # Author: Bruce Tesar
 
+require 'rspec'
 require 'feature'
 
 RSpec.describe Feature do
-  context "A new Feature of type GROG" do
-    before(:each) do
-      @feature = Feature.new("GROG")
-    end
-
-    it "is unset" do
-      expect(@feature.unset?).to be true
-    end
-    it "has type GROG" do
-      expect(@feature.type).to eq("GROG")
-    end
-    it "has value Feature::UNSET" do
-      expect(@feature.value).to eq(Feature::UNSET)
-    end
-    
-    context "set to value KILB" do
-      before(:each) do
-        @feature.value = "KILB"
-      end
-      it "is not unset" do
-        expect(@feature.unset?).to be false
-      end
-      it "has value KILB" do
-        expect(@feature.value).to eq("KILB")
-      end
-      context "with another feature with type GROG and value KILB" do
-        before(:each) do
-          @feat2 = Feature.new("GROG")
-          @feat2.value = "KILB"          
-        end
-        it "the features are ==" do
-          expect(@feature==@feat2).to be true
-        end
-        it "the features are eql?" do
-          expect(@feature.eql?(@feat2)).to be true
-        end
-      end
-      context "with another feature with value BORK" do
-        before(:each) do
-          @feat2 = Feature.new("GROG")
-          @feat2.value = "BORK"          
-        end
-        it "the features are not ==" do
-          expect(@feature==@feat2).to be false
-        end
-        it "the features are not eql?" do
-          expect(@feature.eql?(@feat2)).to be false
-        end
-      end
-      context "with another feature with type OOPH" do
-        before(:each) do
-          @feat2 = Feature.new("OOPH")
-          @feat2.value = "KILB"          
-        end
-        it "the features are not ==" do
-          expect(@feature==@feat2).to be false
-        end
-        it "the features are not eql?" do
-          expect(@feature.eql?(@feat2)).to be false
-        end
+  let(:host_class) do
+    Class.new do
+      include Feature
+      feature_value :val1
+      feature_value :val2
+      define_method :initialize do
+        @value = Feature::UNSET
+        @type = :type_name
+        @value_list = [:val1, :val2].freeze
       end
     end
   end
-end # describe Feature
+  before(:example) do
+    @host = host_class.new
+  end
+  it 'has a value list' do
+    expect(@host.value_list).to contain_exactly(:val1, :val2)
+  end
+  it 'returns its type' do
+    expect(@host.type).to eq :type_name
+  end
+  it 'is initially unset' do
+    expect(@host).to be_unset
+  end
+  it 'is not val1' do
+    expect(@host.val1?).to be false
+  end
+  it 'is not val2' do
+    expect(@host.val2?).to be false
+  end
+  it 'has valid feature value :val1' do
+    expect(@host.valid_value?(:val1)).to be true
+  end
+  it 'rejects :val7 as an invalid value' do
+    expect(@host.valid_value?(:val7)).to be false
+  end
+  it 'yields the valid feature values' do
+    expect { |probe| @host.each_value(&probe) }.to\
+      yield_successive_args(:val1, :val2)
+  end
+
+  context 'when value is set to val1' do
+    before(:example) do
+      @host.set_val1
+    end
+    it 'is val1' do
+      expect(@host.val1?).to be true
+    end
+    it 'is not val2' do
+      expect(@host.val2?).to be false
+    end
+  end
+
+  context 'and another feature of same type and value' do
+    let(:feature2) { double('feature 2') }
+    before(:example) do
+      allow(feature2).to receive(:type).and_return(:type_name)
+      allow(feature2).to receive(:value).and_return(Feature::UNSET)
+    end
+    it 'are ==' do
+      expect(@host == feature2).to be true
+    end
+    it 'are eql?' do
+      expect(@host.eql?(feature2)).to be true
+    end
+  end
+  context 'and another feature with a different value' do
+    let(:feature2) { double('feature 2') }
+    before(:example) do
+      allow(feature2).to receive(:type).and_return(:type_name)
+      allow(feature2).to receive(:value).and_return(:val2)
+    end
+    it 'are not ==' do
+      expect(@host == feature2).not_to be true
+    end
+    it 'are not eql?' do
+      expect(@host.eql?(feature2)).not_to be true
+    end
+  end
+end
